6803999808adc7a4deee58a5eecb18251b850a79
Krishnasai <krishnasai71100@gmail.com>
2025-01-25 18:26:07 +0100

    Creating Tables in Database fro Accounts, Categories, Transactions

---
+++ fintrakk/finance/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (('Income', 'Income'), ('Expense', 'Expense'))
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.ForeignKey('Account', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    type = models.CharField(choices=TRANSACTION_TYPES, max_length=10)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()  # Removed auto_now_add=True
+
+    def __str__(self):
+        return f"{self.type} - {self.amount}"
